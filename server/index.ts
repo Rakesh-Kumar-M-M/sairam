58f295c6d8db10ea74ca9b3ffd0b78487c64d60c
Muthu Rakesh <rakesh883872@gmail.com>
2025-08-03 16:54:51 +0530

    admin connected

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,22 @@ import { setupVite, serveStatic, log } from "./vite";
 import { connectToMongoDB, getConnectionStatus } from "./mongodb";
 
 const app = express();
+
+// Add CORS middleware for development
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5174');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
